92a8515d5d3520fc169b9f8286c18504d252f0b9
Germain Buchet <germain.buch@gmail.com>
2022-08-05 14:43:05 +0200

    menu added plus about me

--- app/src/main/java/com/technipixl/timefighter/MainActivity.kt
+++ app/src/main/java/com/technipixl/timefighter/MainActivity.kt
@@ -3,11 +3,23 @@ package com.technipixl.timefighter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+<<<<<<< Updated upstream
+=======
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+>>>>>>> Stashed changes
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import java.nio.file.attribute.AclEntry
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
 
     private var score = 0
 
@@ -89,4 +101,53 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, getString(R.string.game_over_message,score), Toast.LENGTH_LONG).show()
         resetGame()
     }
+<<<<<<< Updated upstream
 }
+=======
+
+    private fun showInfo(){
+        val dialogTitle = getString(R.string.about_title,BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
+    }
+
+    companion object{
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeLeft)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState : Saving Score: $score & Time left : $timeLeft")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG,"onDestroy called.")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+         super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId  == R.id.about_item){
+            showInfo()
+        }
+        return true
+    }
+}
+>>>>>>> Stashed changes
